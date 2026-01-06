e85ff252c9a0c64e2e245a6907aadfa3abc7d226
Fran <mfranfc@gmail.com>
2026-01-06 17:34:33 -0500

    Añadir requerimientos

---
+++ src/extract.py
@@ -0,0 +1,13 @@
+#=== LIBRERIAS ===#
+import pandas as pd
+#=================#
+'''
+# TODOS:
+# TODO: Cargar el CSV y entender la estructura
+# TODO: Identificar columnas, tipos de datos, valores nulos
+# TODO: Detectar duplicados y anomalías
+# TODO: Documentar hallazgos
+'''
+
+# TODO: Cargar el CSV y entender la estructura
+
